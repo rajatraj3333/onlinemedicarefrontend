0235c5a0f62d1e44b443355821ab36cd007d6943
rajatraj3333 <rajatkumar108@hotmail.com>
2025-02-21 15:31:32 +0530

    fixes base url of api

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
   const api  =
   axios.create({
-    baseURL: '/api',
+    baseURL: 'https://omcbackend.onrender.com/api',
     headers: {
       'Content-Type': 'application/json'
     }
