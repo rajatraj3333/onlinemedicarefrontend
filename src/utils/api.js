db87f28addc987d3da7cc24860986b92b7a33626
rajatraj3333 <rajatkumar108@hotmail.com>
2025-06-18 23:55:14 +0530

    adding default value to api header

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,7 @@ import axios from "axios";
   const api  =
   axios.create({
     baseURL: 'https://omcbackend.onrender.com/api',
+    withCredentials: true,
     headers: {
       'Content-Type': 'application/json',
       },
