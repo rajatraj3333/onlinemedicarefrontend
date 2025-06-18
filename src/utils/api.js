6b7dd445c40c53650d1fcabaf2d6ab8c5fee5f9d
rajatraj3333 <rajatkumar108@hotmail.com>
2025-06-18 23:40:08 +0530

    fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,6 @@ import axios from "axios";
   const api  =
   axios.create({
     baseURL: 'https://omcbackend.onrender.com/api',
-    withCredentials: true,
     headers: {
       'Content-Type': 'application/json',
       },
